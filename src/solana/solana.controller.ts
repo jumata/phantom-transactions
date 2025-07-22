5aaa54cd8b4bd4f9d8b20341115d545fdec51d15
Jumata96 <54186761+Jumata96@users.noreply.github.com>
2025-07-22 17:59:46 -0500

    added solana controller

--- src/solana/solana.controller.ts
+++ src/solana/solana.controller.ts
@@ -1,4 +1,16 @@
-import { Controller } from '@nestjs/common';
+import { Controller, Post, Body } from '@nestjs/common';
+import { SolanaService } from './solana.service';
 
 @Controller('solana')
-export class SolanaController {}
+export class SolanaController {
+  constructor(private readonly solanaService: SolanaService) {}
+
+  @Post('send-token')
+  async sendToken(
+    @Body('senderKey') senderKey: string,
+    @Body('recipientAddress') recipientAddress: string,
+    @Body('amount') amount: number,
+  ) {
+    return await this.solanaService.sendTransaction(senderKey, recipientAddress, amount);
+  }
+}
